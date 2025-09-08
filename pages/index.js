ade856995a7e895c58fb0a321ad24060f723e275
Guilherme Menezes <127321553+imguilhermemenezes@users.noreply.github.com>
2025-09-08 21:27:39 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Não sobra nada pro betinha, it's over, brutal</h1>
+    return <h1>Não sobra nada pro betinha, it's over, brutal, moggado pelo chad</h1>
 }
 
 export default Home;
