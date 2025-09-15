5054827d758519430c3a9d9d497d48f510a37950
Guilherme Menezes <127321553+imguilhermemenezes@users.noreply.github.com>
2025-09-15 20:51:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-    return <h1>Não sobra nada pro betinha, it's over, brutal, moggado pelo chad</h1>
+  return (
+    <h1>Não sobra nada pro betinha, it's over, brutal, moggado pelo chad</h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
